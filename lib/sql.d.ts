2cb0de2610100626999c70655b6ee8a62d27ce58
vasyas <vasyl@stashuk.com>
2018-03-30 13:25:58 +0300

    lib

---
+++ lib/sql.d.ts
@@ -0,0 +1,42 @@
+export interface Page {
+    rows: any[];
+    totals: {
+        count: number;
+    };
+}
+export interface PageRequest {
+    page: number;
+    size: number;
+    sort: string;
+    order: string;
+}
+export declare type ConnectionSupplier = () => any;
+export declare class Sql {
+    private params;
+    private connectionSupplier;
+    parts: string[];
+    constructor(parts: any, params: any, connectionSupplier?: ConnectionSupplier);
+    /** Replace array params (i.e. in(${[1, 2]})) with multiple params, b/c node-mysql2 doesn't support it */
+    private handleArrayParams();
+    append(right: string | Sql, connectionSupplier?: ConnectionSupplier): Sql;
+    wrap(left: Sql, right: Sql): Sql;
+    count(): Promise<number>;
+    aggregate<T extends {
+        [x: string]: string;
+    }>(aggregations: T): Promise<{
+        [x in keyof T]: number;
+    }>;
+    one(def?: any): Promise<any>;
+    scalar(): Promise<any>;
+    private getQueryParams();
+    private getQuery();
+    all(): Promise<any[]>;
+    private convertTypesOnReading(rows, fields);
+    insert(): Promise<number>;
+    update(): Promise<void>;
+    page(request: PageRequest, totalAggregations?: {
+        count: string;
+        [x: string]: string;
+    }): Promise<Page>;
+}
+export declare function sql(parts: any, ...params: any[]): Sql;
